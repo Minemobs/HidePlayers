06293e46402965a46c2ab51db0e629cf81c16d12
minemobs <issahakim94@gmail.com>
2021-12-19 16:11:05 +0100

    Updated to 1.18.1

--- src/main/java/fr/minemobs/hideplayers/HidePlayers.java
+++ src/main/java/fr/minemobs/hideplayers/HidePlayers.java
@@ -5,28 +5,29 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.lwjgl.glfw.GLFW;
 
 public class HidePlayers implements ModInitializer {
 
     private static boolean renderPlayer;
-    private KeyBinding renderPlayerKey;
 
     @Override
     public void onInitialize() {
-        var LOGGER = LogFactory.getLog("HidePlayers");
+        Log logger = LogFactory.getLog("HidePlayers");
         renderPlayer = true;
-        renderPlayerKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-                "key.hideplayers.hideplayers",
-                InputUtil.Type.KEYSYM,
-                GLFW.GLFW_KEY_H,
-                "key.hideplayers.hideplayers"
+        KeyBinding renderPlayerKey = KeyBindingHelper.registerKeyBinding(
+                new KeyBinding(
+                        "key.hideplayers.hideplayers",
+                    InputUtil.Type.KEYSYM,
+                    GLFW.GLFW_KEY_H,
+                    "key.hideplayers.hideplayers"
         ));
         ClientTickEvents.END_CLIENT_TICK.register(unused -> {
             while(renderPlayerKey.wasPressed()) {
                 renderPlayer = !renderPlayer;
-                LOGGER.info(renderPlayer);
+                logger.info(renderPlayer);
             }
         });
     }
